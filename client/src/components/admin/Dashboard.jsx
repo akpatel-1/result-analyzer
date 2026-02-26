ae21781556260b8dcda68ac1e9080371574af741
akpatel-1 <akpatel.work1@gmail.com>
2026-02-26 21:40:29 +0530

    setup: configure tailwind css

--- client/src/components/admin/Dashboard.jsx
+++ client/src/components/admin/Dashboard.jsx
@@ -1,3 +1,7 @@
 export default function Dashboard() {
-  return <h1>Hello Wordl!</h1>;
+  return (
+    <div className="bg-orange-400">
+      <h1 className="font-bold text-5xl">Hello Wordl!</h1>
+    </div>
+  );
 }
