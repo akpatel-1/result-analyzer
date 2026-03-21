22de6ef729251f6150b45d41863cd407a7c8b79b
akpatel-1 <akpatel.work1@gmail.com>
2026-03-21 21:21:57 +0530

    feat: add error handling pages for 403, 404, and 500 errors

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import { adminRoutes } from './router/admin.routes';
+import { sharedRoutes } from './router/shared.routes';
 
-export const router = createBrowserRouter(adminRoutes);
+export const router = createBrowserRouter(sharedRoutes);
