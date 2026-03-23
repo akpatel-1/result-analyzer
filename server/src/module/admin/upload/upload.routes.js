7fabbcc191eedc15d3b2e07edd0a4e5a956083e6
akpatel-1 <akpatel.work1@gmail.com>
2026-03-24 03:28:43 +0530

    refactor: remove multer middleware and adjust profile upload route

--- server/src/module/admin/upload/upload.routes.js
+++ server/src/module/admin/upload/upload.routes.js
@@ -3,7 +3,6 @@ import express from 'express';
 import { validateSchema } from '../../validate.schema.middleware.js';
 import { validateSession } from '../session/session.middleware.js';
 import { controller } from './upload.controller.js';
-import { middleware } from './upload.middleware.js';
 import { schema } from './upload.schema.js';
 
 export const uploadRoute = express.Router();
@@ -11,8 +10,6 @@ export const uploadRoute = express.Router();
 uploadRoute.post(
   '/upload/profile',
   validateSession,
-  middleware.uploadJSON.array('files'),
-  middleware.parseJSONfiles,
-  validateSchema(schema, 'parsedJSON'),
+  validateSchema(schema),
   controller.handleProfileUpload
 );
