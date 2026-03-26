1b91c06bae769117f17c7096fb5286477f7da031
akpatel-1 <akpatel.work1@gmail.com>
2026-03-26 09:19:10 +0530

    refactor: remove timeout configuration from axios clients

--- client/src/lib/axios.instance.js
+++ client/src/lib/axios.instance.js
@@ -3,12 +3,9 @@ import axios from 'axios';
 export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
-  timeout: 10000,
 });
 
 export const studentClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
-  timeout: 10000,
 });
-
